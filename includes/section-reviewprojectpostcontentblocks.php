11fb521dc2bb246197f87a886c2067da99f6adca
martin <martindw97@gmail.com>
2023-04-06 21:54:59 +0100

    border border-1 p-2

--- includes/section-reviewprojectpostcontentblocks.php
+++ includes/section-reviewprojectpostcontentblocks.php
@@ -267,7 +267,7 @@ $PartsProductParagraphImage = get_sub_field('parts_paragraph_image');
 <h4 class="mt-4"><?php echo $PartsProductParagraphTitle; ?><i></i></h4>
 <?php endif; ?>	 
 <?php if ($PartsProductParagraphImage) : ?>
-    <p><?php echo $PartsProductParagraphImage; ?></p>
+    <p class="border border-1 p-2"><?php echo $PartsProductParagraphImage; ?></p>
 <?php endif; ?>    
 <table class="table table-light table-hover table-bordered p-2">
 <tr>
