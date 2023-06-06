9c4ff8461497265fda1508d405a6d7708eee0874
martin <martindw97@gmail.com>
2023-06-06 21:38:44 +0100

    moved adlock v1.1

--- includes/section-home-page-block.php
+++ includes/section-home-page-block.php
@@ -99,23 +99,6 @@ while ($block1->have_posts()) : $block1->the_post();
                                 ?>
 <div class="non_featured_block_home_page">
 
-<?php
-
-$show_ad = get_field('landing_page_block_ad'); // Check if the field value is 'yes'
-
-if (have_rows('home_page_blocks')) {
-    while (have_rows('home_page_blocks')) {
-        the_row();
-        if ($show_ad === 'yes') {
-            get_template_part('landing_page_ads/homepage_header_top');
-        } elseif (get_row_layout() === 'no') {
-            $ad_code = get_sub_field('script');
-            echo $ad_code;
-        }
-    }
-}
-?>
-
 
 <div class="container shadow-sm p-3 mb-2 bg-body rounded" style="height:195px;"><!-- small non-fetured block-->
 
@@ -213,3 +196,19 @@ if (have_rows('home_page_blocks')) {
 </div>
 
 
+<?php
+
+$show_ad = get_field('landing_page_block_ad'); // Check if the field value is 'yes'
+
+if (have_rows('home_page_blocks')) {
+    while (have_rows('home_page_blocks')) {
+        the_row();
+        if ($show_ad === 'yes') {
+            get_template_part('landing_page_ads/homepage_header_top');
+        } elseif (get_row_layout() === 'no') {
+            $ad_code = get_sub_field('script');
+            echo $ad_code;
+        }
+    }
+}
+?>
