c7cd06def8d2d363fda448e0ffc033b9d12e6abe
martin <martindw97@gmail.com>
2023-06-11 21:05:05 +0100

    fix image output

--- includes/section-meta.php
+++ includes/section-meta.php
@@ -6,7 +6,8 @@
       // Get the ACF field value for author_profile_image
   $author_profile_image = get_field('author_profile_image');
   ?>
-      <img src="'. $author_profile_image .'" class="img-fluid rounded-start" alt="">
+     <img src="<?php echo $author_profile_image; ?>" class="img-fluid rounded-start" alt="">
+
     </div>
     <div class="col-md-8">
       <div class="card-body">
