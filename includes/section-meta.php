4f94952b8888c5f5286e56c531dbd0b829d6ebdc
martin <martindw97@gmail.com>
2023-06-11 21:41:51 +0100

    updated bg border and padding

--- includes/section-meta.php
+++ includes/section-meta.php
@@ -1,11 +1,11 @@
-<div class="container-fluid border border-light border border-1 pb-1">
+<div class="container-fluid border border-dark border border-1 pb-1 bg-light">
     <div class="row">
       <div class="col-lg-12 col-md-12 col-sm-12">
         <?php
       // Get the ACF field value for author_profile_image
       $author_profile_image = get_field('author_profile_image');
     ?>
-    <img src="<?php echo $author_profile_image; ?>" style="width:25%; object-fit: cover; object-position: center; border-radius: 50%;">
+    <img class="p-2" src="<?php echo $author_profile_image; ?>" style="width:25%; object-fit: cover; object-position: center; border-radius: 50%;">
 
       </div>
       <div class="col-lg-12 col-md-12 col-sm-12">
