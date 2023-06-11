c30c0137fdb5cdf167eb85f48d52752b9cceac0b
martin <martindw97@gmail.com>
2023-06-11 21:47:25 +0100

    moved category and added tags

--- includes/section-meta.php
+++ includes/section-meta.php
@@ -1,3 +1,32 @@
+<?php
+          $categories = get_the_category();
+          $separator = ' ';
+          $output = '';
+          if ( ! empty( $categories ) ) {
+              foreach( $categories as $category ) {
+                  $output .= '<i>Category: </i><a href="' . esc_url( get_category_link( $category->term_id ) ) . '" alt="' . esc_attr( sprintf( __( 'View all posts in %s', 'textdomain' ), $category->name ) ) . '">' . esc_html( $category->name ) . '</a>' . $separator;
+              }
+              echo trim( $output, $separator );
+          }
+?>
+
+<?php
+$tags = get_the_tags(); // Get the tags for the current post
+
+if ($tags) {
+    $tag_names = array(); // Create an empty array to store tag names
+
+    foreach ($tags as $tag) {
+        $tag_names[] = $tag->name; // Add each tag name to the array
+    }
+
+    $tags_string = implode(', ', $tag_names); // Combine the tag names with a comma and space
+
+    echo $tags_string; // Output the comma-separated tags
+}
+?>
+
+
 <div class="container-fluid border border-dark border border-1 pb-1 bg-light">
     <div class="row">
       <div class="col-lg-12 col-md-12 col-sm-12">
@@ -8,7 +37,14 @@
     <img class="p-2" src="<?php echo $author_profile_image; ?>" style="width:25%; object-fit: cover; object-position: center; border-radius: 50%;">
 
       </div>
-      
+      <div class="col-lg-12 col-md-12 col-sm-12">
+       <div class="card-body">
+        <h5 class="card-title">By <?php the_author_meta('display_name', 1); ?></h5>
+        <p class="card-text"><i>Posted </i><?php echo get_the_date();?></p>
+        <p class="card-text"><small class="text-muted">
+        </small></p>
+      </div>
+      </div>
       <div class="col-lg-12 col-md-12 col-sm-12">
         <?php
 $author_id = get_the_author_meta('ID');
