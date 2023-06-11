7ed84a84733e61bfcb70f6c11947abc375681b80
martin <martindw97@gmail.com>
2023-06-11 22:03:55 +0100

    test to output tags a categories at buttons

--- includes/section-post-meta.php
+++ includes/section-post-meta.php
@@ -1,13 +1,14 @@
 <?php
-          $categories = get_the_category();
-          $separator = ' ';
-          $output = '';
-          if ( ! empty( $categories ) ) {
-              foreach( $categories as $category ) {
-                  $output .= '<i>Category: </i><a href="' . esc_url( get_category_link( $category->term_id ) ) . '" alt="' . esc_attr( sprintf( __( 'View all posts in %s', 'textdomain' ), $category->name ) ) . '">' . esc_html( $category->name ) . '</a>' . $separator;
-              }
-              echo trim( $output, $separator );
-          }
+$categories = get_the_category();
+$separator = ' ';
+$output = '';
+
+if (!empty($categories)) {
+    foreach ($categories as $category) {
+        $output .= '<i>Category: </i><a href="' . esc_url(get_category_link($category->term_id)) . '" alt="' . esc_attr(sprintf(__('View all posts in %s', 'textdomain'), $category->name)) . '" class="btn btn-primary rounded-pill">' . esc_html($category->name) . '</a>' . $separator;
+    }
+    echo trim($output, $separator);
+}
 ?>
 <br>
 <i>Tags: </i>
@@ -23,7 +24,7 @@ if ($tags) {
 
     $tags_string = implode(', ', $tag_names); // Combine the tag names with a comma and space
 
-    echo $tags_string; // Output the comma-separated tags
+    echo '<span class="btn btn-secondary rounded-pill">' . $tags_string . '</span>'; // Output the comma-separated tags
 }
 ?>
 <br><br>
