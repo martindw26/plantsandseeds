958307b6c66edf5473fcbb5427ced7db18034105
martin <martindw97@gmail.com>
2023-06-11 22:05:15 +0100

    test to output tags a categories at buttons v2

--- includes/section-post-meta.php
+++ includes/section-post-meta.php
@@ -2,10 +2,9 @@
 $categories = get_the_category();
 $separator = ' ';
 $output = '';
-
 if (!empty($categories)) {
     foreach ($categories as $category) {
-        $output .= '<i>Category: </i><a href="' . esc_url(get_category_link($category->term_id)) . '" alt="' . esc_attr(sprintf(__('View all posts in %s', 'textdomain'), $category->name)) . '" class="btn btn-primary rounded-pill">' . esc_html($category->name) . '</a>' . $separator;
+        $output .= '<a href="' . esc_url(get_category_link($category->term_id)) . '" class="btn btn-primary rounded-pill">' . esc_html($category->name) . '</a>' . $separator;
     }
     echo trim($output, $separator);
 }
@@ -16,15 +15,10 @@ if (!empty($categories)) {
 $tags = get_the_tags(); // Get the tags for the current post
 
 if ($tags) {
-    $tag_names = array(); // Create an empty array to store tag names
-
     foreach ($tags as $tag) {
-        $tag_names[] = $tag->name; // Add each tag name to the array
+        echo '<a href="' . esc_url(get_tag_link($tag->term_id)) . '" class="btn btn-secondary rounded-pill">' . esc_html($tag->name) . '</a> ';
     }
-
-    $tags_string = implode(', ', $tag_names); // Combine the tag names with a comma and space
-
-    echo '<span class="btn btn-secondary rounded-pill">' . $tags_string . '</span>'; // Output the comma-separated tags
 }
 ?>
 <br><br>
+
