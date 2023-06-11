da68839bd94518dfe25cd602a46d2dfbea9dd210
martin <martindw97@gmail.com>
2023-06-11 20:21:04 +0100

    rm author bio card

--- about.php
+++ about.php
@@ -27,43 +27,7 @@
           </div>
 
 
-          <?php
-$user_id = get_current_user_id();
-$avatar_url = get_avatar_url($user_id); // Get the user's avatar URL
-$biographical_info = get_user_meta($user_id, 'description', true); // Get the user's biographical info
 
-// Get the author's social media profile URLs
-$facebook_url = get_the_author_meta('facebook', $user_id);
-$twitter_url = get_the_author_meta('twitter', $user_id);
-$instagram_url = get_the_author_meta('instagram', $user_id);
-
-?>
-
-<div class="card mb-3" style="">
-  <div class="row g-0">
-    <div class="col-md=3">
-      <img src="<?php echo $avatar_url; ?>" class="img-fluid rounded-start p-2" alt="Profile Picture">
-    </div>
-    <div class="col-md">
-      <div class="card-body">
-        <h5 class="card-title"><?php the_author_meta('display_name', $user_id); ?></h5>
-        <p class="card-text"><?php echo $biographical_info; ?></p>
-
-        <?php if ($facebook_url) : ?>
-          <a class="bi bi-facebook text-light m-2 p-2" href="<?php echo $facebook_url; ?>" target="_blank" rel="nofollow noopener noreferrer">Facebook</a>
-        <?php endif; ?>
-
-        <?php if ($twitter_url) : ?>
-          <a href="<?php echo $twitter_url; ?>" target="_blank" rel="nofollow noopener noreferrer"><p class="bi bi-twitter text-light m-1 p-2"></a>
-        <?php endif; ?>
-
-        <?php if ($instagram_url) : ?>
-          <a href="<?php echo $instagram_url; ?>" target="_blank" rel="nofollow noopener noreferrer">Instagram</a>
-        <?php endif; ?>
-      </div>
-    </div>
-  </div>
-</div>
 
 
 
