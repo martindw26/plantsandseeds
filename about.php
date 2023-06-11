729af3bf92344fd5c3bd82bd39b8bce2230b42c7
martin <martindw97@gmail.com>
2023-06-11 19:26:40 +0100

    added social links to bio card

--- about.php
+++ about.php
@@ -32,6 +32,11 @@ $user_id = get_current_user_id();
 $avatar_url = get_avatar_url($user_id); // Get the user's avatar URL
 $biographical_info = get_user_meta($user_id, 'description', true); // Get the user's biographical info
 
+// Get the author's social media profile URLs
+$facebook_url = get_the_author_meta('facebook', $user_id);
+$twitter_url = get_the_author_meta('twitter', $user_id);
+$instagram_url = get_the_author_meta('instagram', $user_id);
+
 ?>
 
 <div class="card mb-3" style="">
@@ -43,10 +48,23 @@ $biographical_info = get_user_meta($user_id, 'description', true); // Get the us
       <div class="card-body">
         <h5 class="card-title"><?php the_author_meta('display_name', $user_id); ?></h5>
         <p class="card-text"><?php echo $biographical_info; ?></p>
+
+        <?php if ($facebook_url) : ?>
+          <a href="<?php echo $facebook_url; ?>" target="_blank" rel="nofollow noopener noreferrer">Facebook</a>
+        <?php endif; ?>
+
+        <?php if ($twitter_url) : ?>
+          <a href="<?php echo $twitter_url; ?>" target="_blank" rel="nofollow noopener noreferrer">Twitter</a>
+        <?php endif; ?>
+
+        <?php if ($instagram_url) : ?>
+          <a href="<?php echo $instagram_url; ?>" target="_blank" rel="nofollow noopener noreferrer">Instagram</a>
+        <?php endif; ?>
       </div>
     </div>
   </div>
 </div>
+
 
 
           </div>
