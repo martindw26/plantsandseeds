1a7ec9bac5daa5575b102d0f4f28375551b75281
martin williams <martindw97@gmail.com>
2023-07-01 16:14:01 +0100

    updated tags meta block

--- includes/section-post-meta.php
+++ includes/section-post-meta.php
@@ -12,14 +12,14 @@ if (!empty($categories)) {
 }
 ?>
 
-<h5 class="display-6 pt-4 pb-4">Tags: </h5>
+<h4>Tags: </h4>
 
 <?php
 $tags = get_the_tags(); // Get the tags for the current post
 
 if ($tags) {
     foreach ($tags as $tag) {
-        echo '<a href="' . esc_url(get_tag_link($tag->term_id)) . '" class="btn btn-primary rounded-pill mb-4 mb-md-0">' . esc_html($tag->name) . '</a> ';
+        echo '<a href="' . esc_url(get_tag_link($tag->term_id)) . '"<span class="badge bg-primary p-2 text-center fs-6">' . esc_html($tag->name) . '</a></span>';
     }
 }
 ?>
