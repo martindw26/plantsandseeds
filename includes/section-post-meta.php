d71c30b09b3cfa8450428c07061e2e91f372e0a4
Martin W <martindw97@gmail.com>
2023-07-19 21:48:30 +0100

    tag-cat-btns hover white

--- includes/section-post-meta.php
+++ includes/section-post-meta.php
@@ -7,7 +7,7 @@ $separator = ' ';
 $output = '';
 if (!empty($categories)) {
     foreach ($categories as $category) {
-        $output .= '<a href="' . esc_url(get_category_link($category->term_id)) . '"<span class="badge bg-secondary p-2 text-center fs-6 me-2 ms-2 text-decoration-none">' . esc_html($category->name) . '</a></span>' . $separator;
+        $output .= '<a class="tag-cat-btns" href="' . esc_url(get_category_link($category->term_id)) . '"<span class="badge bg-secondary p-2 text-center fs-6 me-2 ms-2 text-decoration-none">' . esc_html($category->name) . '</a></span>' . $separator;
     }
     echo trim($output, $separator);
 }
@@ -20,7 +20,7 @@ $tags = get_the_tags(); // Get the tags for the current post
 
 if ($tags) {
     foreach ($tags as $tag) {
-        echo '<a href="' . esc_url(get_tag_link($tag->term_id)) . '"<span class="badge bg-secondary p-2 text-center fs-6 me-2 ms-2 text-decoration-none">' . esc_html($tag->name) . '</a></span>';
+        echo '<a class="tag-cat-btns" href="' . esc_url(get_tag_link($tag->term_id)) . '"<span class="badge bg-secondary p-2 text-center fs-6 me-2 ms-2 text-decoration-none">' . esc_html($tag->name) . '</a></span>';
     }
 }
 ?>
