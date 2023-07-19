7829ae7bf7f175661dfe76595f0f748772a374a5
martin williams <86750566+martindw26@users.noreply.github.com>
2023-07-19 23:19:03 +0100

    Update section-post-meta.php
    rm auto

--- includes/section-post-meta.php
+++ includes/section-post-meta.php
@@ -22,7 +22,7 @@ $tags = get_the_tags(); // Get the tags for the current post
 if ($tags) {
     echo '<div class="row">';
     foreach ($tags as $tag) {
-        echo '<div class="col-sm col-md-auto">
+        echo '<div class="col-sm col-md">
             <a class="tag-cat-btns" href="' . esc_url( get_tag_link( $tag->term_id ) ) . '">
                 <span class="badge bg-secondary p-2 text-center fs-6 me-2 ms-2 mb-2 text-decoration-none">' . esc_html( $tag->name ) . '</span>
             </a>
