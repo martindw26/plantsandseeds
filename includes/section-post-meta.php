aafd3e26a8567bbf3d54b931b24df58b5b927e64
Martin W <martindw97@gmail.com>
2023-07-19 22:21:19 +0100

    $tags = get_the_tags(); // Get the tags for the current post
    if ($tags) {
        echo '<div class="row">';
        foreach ($tags as $tag) {
            echo '<div class="col-sm-12 col-md-6 col-lg-4">';
            echo '<div class="d-flex justify-content-center align-items-center">';
            echo '<a class="tag-cat-btns" href="' . esc_url( get_tag_link( $tag->term_id ) ) . '">';
            echo '<span class="badge bg-secondary p-2 text-center fs-6 me-2 ms-2 mb-2 text-decoration-none">' . esc_html( $tag->name ) . '</span>';
            echo '</a>';
            echo '</div>';
            echo '</div>';
        }
        echo '</div>';
    }

--- includes/section-post-meta.php
+++ includes/section-post-meta.php
@@ -20,16 +20,19 @@ if (!empty($categories)) {
 $tags = get_the_tags(); // Get the tags for the current post
 
 if ($tags) {
+    echo '<div class="row">';
     foreach ($tags as $tag) {
-        echo '<div class="row">
-            <div class="col-sm-12 col-md-auto">
-                <a class="tag-cat-btns" href="' . esc_url( get_tag_link( $tag->term_id ) ) . '">
-                    <span class="badge bg-secondary p-2 text-center fs-6 me-2 ms-2 mb-2 text-decoration-none">' . esc_html( $tag->name ) . '</span>
-                </a>
-            </div>
-        </div>';
+        echo '<div class="col-sm-12 col-md-6 col-lg-4">';
+        echo '<div class="d-flex justify-content-center align-items-center">';
+        echo '<a class="tag-cat-btns" href="' . esc_url( get_tag_link( $tag->term_id ) ) . '">';
+        echo '<span class="badge bg-secondary p-2 text-center fs-6 me-2 ms-2 mb-2 text-decoration-none">' . esc_html( $tag->name ) . '</span>';
+        echo '</a>';
+        echo '</div>';
+        echo '</div>';
     }
+    echo '</div>';
 }
+
 
 
 
