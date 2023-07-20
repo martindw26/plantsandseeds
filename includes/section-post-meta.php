ede4c2499c89865dd22e8e9e632f1200d541080a
Martin W <martindw97@gmail.com>
2023-07-20 17:54:52 +0100

    text white

--- includes/section-post-meta.php
+++ includes/section-post-meta.php
@@ -26,7 +26,7 @@ if ($tags) {
         <div class="btn-group">
         <button type="button" class="btn btn-primary m-1">
             <a class="tag-cat-btns" href="' . esc_url( get_tag_link( $tag->term_id ) ) . '">
-                <span class="text-decoration-none">' . esc_html( $tag->name ) . '</span>
+                <span class="text-decoration-none text-white">' . esc_html( $tag->name ) . '</span>
             </a>
         </button>
         </div>';
