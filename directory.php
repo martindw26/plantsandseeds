5a32ee6820350720a7f7ab8add476bd2f18ce797
Martin W <martindw97@gmail.com>
2023-07-29 19:33:07 +0100

    Updated directory page

--- directory.php
+++ directory.php
@@ -1,85 +1,74 @@
 <?php
-  get_header();
- /* Template Name: Directiry Post*/
- /* Template Post Type: post */
-?>  
+/*
+Template Name: Directory Post
+Template Post Type: page
+*/
+
+get_header();
+?>
 
 <div class="container">
 
-        <div class="row">
+    <div class="row">
 
-          <div class="col-lg-8 p-lg-2 bg-white text-dark p-1">
-	
+        <div class="col-lg-8 p-lg-2 bg-white text-dark p-1">
 
-            <h1 class="display-2 font-weight-bold"><?php echo esc_html( get_the_title() );?></h1>
-					
-				<!-- Social share icons-->
-				<?php $enable_in_article = get_field('on_article','option');?>
-				<?php if($enable_in_article):?>
-					<?php get_template_part('includes/section','social_sharefrontpagearticle');?>
-				<?php else:?>
-					<?php echo '<style>{ display:none;}</style>';?>
-				<?php endif;?>
-				<!-- End Social share icons-->
+            <h1 class="display-2 font-weight-bold"><?php echo esc_html(get_the_title()); ?></h1>
 
+            <!-- Social share icons-->
+            <?php $enable_in_article = get_field('on_article', 'option'); ?>
+            <?php if ($enable_in_article) : ?>
+                <?php get_template_part('includes/section', 'social_sharefrontpagearticle'); ?>
+            <?php else : ?>
+                <?php echo '<style>{ display:none;}</style>'; ?>
+            <?php endif; ?>
+            <!-- End Social share icons-->
 
+            <!-- header image block-->
+            <?php get_template_part('includes/section', 'header-image block'); ?>
+            <!-- End header image block-->
 
-              <?php get_template_part('includes/section','difficulty_rating');?><br><br>
+	    <?php
+		$seo_paragraph = get_field('seo_paragraph');
 
-             <!-- header image block-->
-                                                
-             <?php get_template_part('includes/section','header-image block');?>
-                                                
-             <!-- End header image block-->
-
-             <!-- SEO text-->
-
-             <div class=" bg-secondary text-light p-4 mb-4 border-dark lead">
-             <?php the_field('seo_paragraph');?> 
-             </div>
-
-             <!-- Article content blocks -->
-             <?php get_template_part('includes/section','directory');?>
-             <!-- End Article content blocks-->
-
-             <!-- meta block-->
-             <?php get_template_part('includes/section','meta');?>
-             <!-- End meta block-->
-
-             <!-- Social share icons-->
-             <?php $enable_in_article = get_field('on_article','option');?>
-             <?php if($enable_in_article):?>
-             <?php get_template_part('includes/section','social_sharefrontpagearticle');?>
-             <?php else:?>
-             <?php echo '<style>{ display:none;}</style>';?>
-             <?php endif;?>
-              <hr>
-             <!-- End Social share icons-->
-
-             <!-- Start of posts related posts-->
-             <?php get_template_part( 'template-parts/related_new' );?>
-             <!-- End of posts related posts-->
-
-          </div>
-
-                        <div class="sidebar col-lg-4 col">
-
-                                      <!-- Category sidebar search-->
-                                      <?php get_template_part('includes/section','search');?>
-                                      <!-- End Category sidebar search-->
-
-		   <!-- Start of posts sidebar related posts-->
-	 	   <?php get_template_part( 'template-parts/projects_sidebar' );?>
-		   <!-- End of posts sidebar related posts-->     
-                        </div>
+		if (!empty($seo_paragraph)):
+	    ?>
+		<div class="bg-secondary text-light p-4 mb-4 border-dark lead">
+		    <?php echo $seo_paragraph; ?>
+		</div>
+	        <?php endif; ?>
 
 
+            <!-- Article content blocks -->
+            <?php get_template_part('includes/section', 'directory'); ?>
+            <!-- End Article content blocks-->
 
+            <!-- meta block-->
+            <?php get_template_part('includes/section', 'meta'); ?>
+            <!-- End meta block-->
 
-      </div>               
+            <!-- Social share icons-->
+            <?php $enable_in_article = get_field('on_article', 'option'); ?>
+            <?php if ($enable_in_article) : ?>
+                <?php get_template_part('includes/section', 'social_sharefrontpagearticle'); ?>
+            <?php else : ?>
+                <?php echo '<style>{ display:none;}</style>'; ?>
+            <?php endif; ?>
+            <hr>
+            <!-- End Social share icons-->
+        </div>
 
+        <div class="sidebar col-lg-4 col">
+
+            <!-- Category sidebar search-->
+            <?php get_template_part('includes/section', 'search'); ?>
+            <!-- End Category sidebar search-->
+
+            <!-- Start of posts sidebar related posts-->
+            <?php get_template_part('template-parts/projects_sidebar'); ?>
+            <!-- End of posts sidebar related posts-->
+        </div>
+    </div>
 </div>
-
-    
 
 <?php get_footer(); ?>
