1779624131abbd95bc5b290a15c0dc7741443539
Martin W <martindw97@gmail.com>
2023-08-01 22:47:52 +0100

    navbar-fluid navbar-expand-md navbar-black bg-black pt-1

--- includes/section-main_nav.php
+++ includes/section-main_nav.php
@@ -1,6 +1,6 @@
 <!-- Main navigation -->
 
-<nav class="navbar-fluid navbar-expand-md navbar-black bg-black" role="navigation">
+<nav class="navbar-fluid navbar-expand-md navbar-black bg-black pt-1" role="navigation">
 <!-- Brand and toggle get grouped for better mobile display -->
 <button class="navbar-toggler text-start" type="button" data-toggle="collapse" data-target="#navbar-collapse-1" aria-controls="navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
 <span class="navbar-toggler-icon"><h5 class="	fas fa-bars"></h5>
