07b647aa19ca10207338f8efa225ed8edc819a03
Martin W <martindw97@gmail.com>
2023-08-04 23:02:24 +0100

    author update 3

---
+++ includes/section-meta.php
@@ -0,0 +1,29 @@
+<div class="container-fluid border border-dark border-1 pb-1 bg-light">
+    <div class="row">
+        <div class="card mb-3 rounded-0" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-4" style="padding:0;">
+                    <?php
+                    // Get the ACF field value for author_profile_image
+                    $author_profile_image = get_field('author_profile_image');
+                    if(!empty($author_profile_image)): ?>
+                        <img src="<?php echo esc_url($author_profile_image); ?>" style="width: 100%; height: 100%; object-fit: cover;" class="img-fluid rounded-0" alt="Author Image">
+                    <?php endif; ?>
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">By <?php the_author_meta('display_name', 1); ?></h5>
+                        <p class="card-text">
+                            <?php 
+                            $author_id = get_the_author_meta('ID', 1);
+                            $author_bio = get_the_author_meta('description', $author_id);
+                            echo esc_html($author_bio);
+                            ?>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr>
+</div>
