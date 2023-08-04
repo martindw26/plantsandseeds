531edc9ca0f5b86e87038835312b89101a00e2a7
Martin W <martindw97@gmail.com>
2023-08-04 23:29:21 +0100

    <div class="bi bi-person-fill-add text-dark p-2" width="48" height="48"></div>

--- includes/section-meta.php
+++ includes/section-meta.php
@@ -21,6 +21,7 @@
                             ?>
                             <!-- social icons -->
                             <section class="d-flex justify-content-start align-items-center">
+                            <div class="bi bi-person-fill-add text-dark p-2" width="48" height="48"></div>
                             <a class="bi bi-facebook text-dark me-4" width="48" height="48" href="<?php the_field('facebook','option'); ?>" target="_blank"></a>
                             <a class="bi bi-twitter text-dark me-4" width="48" height="48" href="<?php the_field('twitter','option'); ?>" target="_blank"></a>
                             <a class="bi bi-reddit text-dark me-4" width="48" height="48" href="<?php the_field('reddit','option'); ?>" target="_blank"></a>
