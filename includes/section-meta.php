504a7690cea2f8d0eb86065392b5b836a2ffc07f
Martin W <martindw97@gmail.com>
2023-08-04 22:58:20 +0100

    author box

--- includes/section-meta.php
+++ includes/section-meta.php
@@ -1,29 +1,29 @@
-<div class="container-fluid border border-dark border border-1 pb-1 bg-light">
+<div class="container-fluid border border-dark border-1 pb-1 bg-light">
     <div class="row">
-      <div class="col-lg-12 col-md-12 col-sm-12">
-        <?php
-      // Get the ACF field value for author_profile_image
-      $author_profile_image = get_field('author_profile_image');
-    ?>
-    <img class="p-2" src="<?php echo $author_profile_image; ?>" style="width:25%; object-fit: cover; object-position: center; border-radius: 50%;">
-
-      </div>
-      <div class="col-lg-12 col-md-12 col-sm-12">
-       <div class="card-body">
-        <h5 class="card-title">By <?php the_author_meta('display_name', 1); ?></h5>
-        <p class="card-text"><i>Posted </i><?php echo get_the_date();?></p>
-        <p class="card-text"><small class="text-muted">
-        </small></p>
-      </div>
-      </div>
-      <div class="col-lg-12 col-md-12 col-sm-12">
-        <?php
-$author_id = get_the_author_meta('ID');
-$author_bio = get_the_author_meta('description', $author_id);
-
-echo $author_bio;
-?>
-      </div>
+        <div class="card mb-3 rounded-0" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-4" style="padding:0;">
+                    <?php
+                    // Get the ACF field value for author_profile_image
+                    $author_profile_image = get_field('author_profile_image');
+                    if(!empty($author_profile_image)): ?>
+                        <img src="<?php echo esc_url($author_profile_image); ?>" style="width: 100%; height: 100%; object-fit: cover;" class="img-fluid rounded-0" alt="Author Image">
+                    <?php endif; ?>
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">By <?php the_author_meta('display_name', 1); ?></h5>
+                        <p class="card-text">
+                            <?php 
+                            $author_id = get_the_author_meta('ID', 1);
+                            $author_bio = get_the_author_meta('description', $author_id);
+                            echo esc_html($author_bio);
+                            ?>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-  <hr>
+    <hr>
+</div>
