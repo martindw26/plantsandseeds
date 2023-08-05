1fa460030ad7211824de75d0f5466888d8afcdae
Martin W <martindw97@gmail.com>
2023-08-05 17:06:54 +0100

    jump points

---
+++ includes/functions/section-jump-points.php
@@ -0,0 +1,35 @@
+<style>
+/* Custom CSS for the horizontal scroller */
+.horizontal-scroll {
+  display: flex;
+  flex-wrap: nowrap;
+  overflow-x: auto;
+  -webkit-overflow-scrolling: touch; /* For smooth scrolling on iOS devices */
+  padding-bottom: 15px; /* Optional: Add some padding at the bottom to accommodate the scrollbar */
+}
+
+/* Optional: Add styles for individual items */
+.horizontal-scroll .col-3 {
+  /* Customize styles for each item in the scroller */
+  /* For example: */
+  padding: 10px;
+  background-color: #f5f5f5;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin-right: 10px;
+}
+
+</style>
+
+             
+            <div class="container">
+            <div class="row horizontal-scroll">
+                <!-- Add your scrollable content here -->
+                <div class="col-6 col-sm-3">Item 1</div>
+                <div class="col-6 col-sm-3">Item 2</div>
+                <div class="col-6 col-sm-3">Item 3</div>
+                <div class="col-6 col-sm-3">Item 4</div>
+                <div class="col-6 col-sm-3">Item 5</div>
+                <!-- Add more items as needed -->
+            </div>
+            </div>
