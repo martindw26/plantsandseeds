2175aaeb08b8a34ddf66a2afc02d1244d6666476
Martin W <martindw97@gmail.com>
2023-08-05 19:56:03 +0100

    add padding

--- includes/section-jump-points.php
+++ includes/section-jump-points.php
@@ -45,7 +45,7 @@ a.row.horizontal-scroll-anchor {
 <?php while (have_rows('jump_points')): the_row(); ?>
 
 <a class="row horizontal-scroll-anchor" href="#">
-<img class="img-fluid" src="<?php echo esc_url(the_sub_field('image')); ?>" style="height:100px; width:200px;">
+<img class="img-fluid" src="<?php echo esc_url(the_sub_field('image')); ?>" style="height:100px; width:200px; padding:2px">
 </a>
 
 
