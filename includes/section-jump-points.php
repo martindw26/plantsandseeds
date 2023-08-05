c20d30c6a3c98f95f077537212f824d77351703d
Martin W <martindw97@gmail.com>
2023-08-05 18:53:47 +0100

    added <i class="bi bi-arrow-down"></i>

--- includes/section-jump-points.php
+++ includes/section-jump-points.php
@@ -41,6 +41,7 @@
 <?php while (have_rows('jump_points')): the_row(); ?>
 
     <img class="img-fluid" src="<?php the_sub_field('image'); ?>" style="height:100px; width:200px;" />
+    <i class="bi bi-arrow-down"></i> 
 
 <?php endwhile; ?>
 
