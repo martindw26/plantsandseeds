243dff8071e48883d7341558713eb57b3712c285
Martin W <martindw97@gmail.com>
2023-08-05 18:52:42 +0100

    revert overlay code

--- includes/section-jump-points.php
+++ includes/section-jump-points.php
@@ -40,12 +40,7 @@
 
 <?php while (have_rows('jump_points')): the_row(); ?>
 
-<div class="image-container">
-  <img class="img-fluid" src="<?php the_sub_field('image'); ?>" style="height: 100px; width: 200px;" />
-
-    <i class="bi bi-arrow-down"></i>
-</div>
-
+    <img class="img-fluid" src="<?php the_sub_field('image'); ?>" style="height:100px; width:200;" />
 
 <?php endwhile; ?>
 
