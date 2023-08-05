949b7ed0ed75d173a790f8e7a0b92c24607c924e
Martin W <martindw97@gmail.com>
2023-08-05 17:03:56 +0100

    added jump point css

--- multi_review3.php
+++ multi_review3.php
@@ -41,6 +41,29 @@
 				      <?php endif;?>
 				      <!-- End Social share icons-->
 
+              <style>
+/* Custom CSS for the horizontal scroller */
+.horizontal-scroll {
+  display: flex;
+  flex-wrap: nowrap;
+  overflow-x: auto;
+  -webkit-overflow-scrolling: touch; /* For smooth scrolling on iOS devices */
+  padding-bottom: 15px; /* Optional: Add some padding at the bottom to accommodate the scrollbar */
+}
+
+/* Optional: Add styles for individual items */
+.horizontal-scroll .col-3 {
+  /* Customize styles for each item in the scroller */
+  /* For example: */
+  padding: 10px;
+  background-color: #f5f5f5;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  margin-right: 10px;
+}
+
+</style>
+
              
             <div class="container">
             <div class="row horizontal-scroll">
