6a625c7c1a1f6ae8ccf909f4df0c19c92f6e5d54
Martin W <martindw97@gmail.com>
2023-08-06 19:11:39 +0100

    <?php $jump_id = get_sub_field('jump_id');?> <span id="<?php echo $jump_id; ?>"></span>

--- includes/section-multiproduct3.php
+++ includes/section-multiproduct3.php
@@ -7,6 +7,9 @@
 <section class="bg-white p-2 mb-4">
 <?php $Product_title = get_sub_field('multi_product_title');?>
 
+<?php $jump_id = get_sub_field('jump_id');?> 
+<span id="<?php echo $jump_id; ?>"></span> 
+
 <h2 class="p-2 card-title font-weight-bold"><u><?php echo $Product_title; ?></u></h2>
 
 <?php $ProductRating = get_sub_field('p_rating');?>
