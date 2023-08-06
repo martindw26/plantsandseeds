c52f2dfc5e11bcd21bf17e346875c13ab0ad90a9
Martin W <martindw97@gmail.com>
2023-08-06 21:40:00 +0100

    Single_buy_now_button

--- includes/section-multiproduct3.php
+++ includes/section-multiproduct3.php
@@ -19,6 +19,7 @@
         <?php endforeach; ?>		
 </div>
 <br>
+<?php $Single_buy_now_button = get_sub_field('buy_now');?>
 <button type="button" class="btn btn-success"><a class="text-white text-decoration-none" href="<?php echo $Single_buy_now_button; ?>" target="_blank"></>Get your <?php echo $Product_title; ?> now</a></button>
 
 <br><br>
