00b31b4f40cb369b6a2941ed55e3c520b1b7f07e
Martin W <martindw97@gmail.com>
2023-08-06 22:27:24 +0100

    scrollbar >> v6

--- includes/section-jump-points.php
+++ includes/section-jump-points.php
@@ -32,9 +32,7 @@
 
 </style>
 
-<?php $jump_point_label = get_field('jump_point_label', 'option'); ?>
-<h5><?php echo $jump_point_label; ?>
-<h6 class="horizontal-scroll-text-mobile">Scroll >></h6></h5>
+<?php $jump_point_label = get_field('jump_point_label', 'option'); ?><h5><?php echo $jump_point_label; ?><h6 class="horizontal-scroll-text-mobile">Scroll >></h6></h5>
 
 <div class="horizontal-scroll">
 <?php if (have_rows('jumps')): ?>
