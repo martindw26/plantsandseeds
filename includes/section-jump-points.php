0d16727bd6b87ad3dc3cdedba0b1aa73088b41e7
Martin W <martindw97@gmail.com>
2023-08-06 17:34:26 +0100

    jump to label v4

--- includes/section-jump-points.php
+++ includes/section-jump-points.php
@@ -46,10 +46,11 @@ scrollbar-color: blue #f1f1f1;
 <?php $jump_to_text = esc_url(get_sub_field('jump_to_text')); ?>
 <?php $image_url = esc_url(get_sub_field('jump_image')); ?>
 
-<span><?php echo $jump_point_label;?></span>
+<span><?php echo $jump_point_label;?>
 <a href="#<?php echo ltrim($anchor, 'http://'); ?>">
 <img class="img-fluid" src="<?php echo $image_url; ?>" style="height:100px; width:200px;">
 </a>
+</span>
 
 <?php endwhile; ?>
 <?php else: ?>
