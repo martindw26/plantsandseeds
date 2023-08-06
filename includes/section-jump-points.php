9a1f57561d5b6eea313a9579a29a9c199b9d440f
Martin W <martindw97@gmail.com>
2023-08-06 16:22:55 +0100

    added anchor URL

--- includes/section-jump-points.php
+++ includes/section-jump-points.php
@@ -47,7 +47,10 @@ a.row.horizontal-scroll-anchor {
 <?php $anchor = esc_url(get_sub_field('jump_anchor')); ?>
 <?php $image_url = esc_url(get_sub_field('jump_image')); ?>
 
-    <img class="img-fluid" src="<?php echo $image_url; ?>" style="height:100px; width:200px;">
+<a href="<?php echo $anchor; ?>">
+  <img class="img-fluid" src="<?php echo $image_url; ?>" style="height:100px; width:200px;">
+</a>
+
 
 <?php endwhile; ?>
 
