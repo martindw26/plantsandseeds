137c01c16903d98266fff725f4d111f01e9bb324
Martin W <martindw97@gmail.com>
2023-08-06 17:09:46 +0100

    update image slider v4

--- includes/section-jump-points.php
+++ includes/section-jump-points.php
@@ -1,28 +1,13 @@
 <style>
-/* Custom CSS for the horizontal scroller */
-.horizontal-scroll {
-  display: flex;
-  flex-wrap: nowrap;
-  overflow-x: auto;
-  -webkit-overflow-scrolling: touch; /* For smooth scrolling on iOS devices */
-  padding-bottom: 15px; /* Optional: Add some padding at the bottom to accommodate the scrollbar */
-}
-
-/* Optional: Add styles for individual items */
-.horizontal-scroll .col-3 {
-  /* Customize styles for each item in the scroller */
-  /* For example: */
+div.scroll-container {
+  background-color: #333;
+  overflow: auto;
+  white-space: nowrap;
   padding: 10px;
-  background-color: #f5f5f5;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  /* Set a fixed width for each item */
-  flex: 0 0 230px;
-  margin-right: 0; /* Remove the right margin */
 }
 
-a.row.horizontal-scroll-anchor {
-  max-width: 230px;
+div.scroll-container img {
+  padding: 10px;
 }
 
 
@@ -50,7 +35,7 @@ a.row.horizontal-scroll-anchor {
 <?php $anchor = esc_url(get_sub_field('jump_anchor')); ?>
 <?php $image_url = esc_url(get_sub_field('jump_image')); ?>
 
-<div>
+<div class="scroll-container">
 <a href="#<?php echo ltrim($anchor, 'http://'); ?>">
 <img class="img-fluid" src="<?php echo $image_url; ?>" style="height:100px; width:200px;">
 </a>
