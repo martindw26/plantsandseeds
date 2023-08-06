94c0a58c0ca7aed1e4612577526c29d37e77abe0
Martin W <martindw97@gmail.com>
2023-08-06 17:17:22 +0100

    updated slider v7

--- includes/section-jump-points.php
+++ includes/section-jump-points.php
@@ -22,28 +22,21 @@
             </div>
             </div>
 -->
-<div class="container">
-            <div class="row horizontal-scroll">
+
+<div class="horizontal-scroll">
 <?php if (have_rows('jumps')): ?>
-
 <?php while (have_rows('jumps')): the_row(); ?>
-
 <?php $anchor = esc_url(get_sub_field('jump_anchor')); ?>
 <?php $image_url = esc_url(get_sub_field('jump_image')); ?>
 
-<div class="scroll-container">
 <a href="#<?php echo ltrim($anchor, 'http://'); ?>">
 <img class="img-fluid" src="<?php echo $image_url; ?>" style="height:100px; width:200px;">
 </a>
-</div>
-
 
 <?php endwhile; ?>
-
 <?php else: ?>
-<!-- If there are no jump points -->
 <p>No jump points found.</p>
 <?php endif; ?>
 </div>
-</div>
+
 
