05048b6ea207f091501be30df09d47283cde4be4
Martin W <martindw97@gmail.com>
2023-08-07 21:49:53 +0100

    update buy now list v2

--- includes/section-multiproduct3.php
+++ includes/section-multiproduct3.php
@@ -76,7 +76,7 @@
 <?php while( have_rows('buy_now_list')): the_row();?>
 
 <tr>
-        <th><img class="img-fluid" src="<?php get_sub_field('item_image');?>"style="width:100px; height:100px; object-fit: cover;"></th>
+        <th><img class="img-fluid" src="<?php echo get_sub_field('item_image');?>" style="width:100px; height:100px; object-fit: cover;"></th>
         <th><?php get_sub_field('item_model');?></th>
         <th><?php get_sub_field('item_price');?></th>
 </tr>				
