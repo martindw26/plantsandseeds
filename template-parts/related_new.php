26e8721cb22fe1d7c6a3e645e68d43319f746b3a
Martin W <martindw97@gmail.com>
2023-08-09 21:43:04 +0100

    updated related posts v2.2

--- template-parts/related_new.php
+++ template-parts/related_new.php
@@ -1,77 +1,78 @@
 <div>
-<h2>You may also <i>like</i></h2>
-<?php
+    <h2>You may also <i>like</i></h2>
+    <?php
+    if(get_field('show_related_posts', 'option') === 'yes') { 
+        // Get tags from the current post
+        $tags = wp_get_post_tags($post->ID);
+        if ($tags) {
+            $tag_ids = array();
+            foreach($tags as $individual_tag) $tag_ids[] = $individual_tag->term_id;
 
-// Check if 'show_related_posts' field is set to 'yes'
-if(get_field('show_related_posts', 'option') === 'yes') { 
-
-    // Get tags from the current post
-    $tags = wp_get_post_tags($post->ID);
-    if ($tags) {
-        $tag_ids = array();
-        foreach($tags as $individual_tag) $tag_ids[] = $individual_tag->term_id;
-
-        $related_order_by = get_field('related_order_by', 'option');
-
-        $args = array(
-            'post_type'     => 'post',
-            'post_status'   => 'publish',
-            'orderby'       => $related_order_by,
-            'tag__in'       => $tag_ids,
-            'post__not_in'  => array($post->ID) // Exclude current post
-        );
-
-        $relatedPosts = new WP_Query($args);
-
-        if($relatedPosts->have_posts()) :
-            while($relatedPosts->have_posts()) : $relatedPosts->the_post(); ?>
-
-                <div class="container p-2 bg-dark text-white">
-                    <div class="row">
-                        <div class="col-lg-9"><h5><a class="text-decoration-none link-dark text-white" href='<?php the_permalink(); ?>'><?php the_title(); ?></a></h5></div>
-                        <div class="col-lg-4"><?php $url = wp_get_attachment_url(get_post_thumbnail_id($post->ID)); ?>
-                            <img class="img-fluid rounded pb-xs-4" src="<?php echo $url ?>" /></div>
-                        <div class="col-lg-6 mb-3 mt-4 mt-md-0"><p class="text-white">&ldquo;<?php echo get_the_excerpt(); ?>&rdquo;</p><br><a class="text-white text-decoration-none align-content-end" href="<?php the_permalink() ?>">Read more →</a></div>
-                    </div>
-                </div><br>
-
-            <?php endwhile;
-            wp_reset_query();
-        else: 
-            // If no related posts by tag, check related posts by post ID
-            $post_id = get_field('post_id');
+            $related_order_by = get_field('related_order_by', 'option');
 
             $args = array(
-                'post_type'         => 'post',
-                'post_status'       => 'publish',
-                'orderby'           => $related_order_by,
-                'post__in'          => $post_id
+                'post_type'     => 'post',
+                'post_status'   => 'publish',
+                'orderby'       => $related_order_by,
+                'tag__in'       => $tag_ids,
+                'post__not_in'  => array($post->ID) // Exclude current post
             );
 
             $relatedPosts = new WP_Query($args);
 
-            if ($relatedPosts-> have_posts()) :
-                while ($relatedPosts->have_posts()) : $relatedPosts->the_post();
-                ?>
-
-                <div class="container p-2 bg-dark text-white">
-                    <div class="row">
-                        <div class="col-lg-9"><h5><a class="text-decoration-none link-dark text-white" href='<?php echo get_permalink();?>'><?php the_title(); ?></a></h5></div>
-                        <div class="col-lg-4"><?php $url = wp_get_attachment_url(get_post_thumbnail_id($post->ID)); ?>
-                            <img class="img-fluid rounded pb-xs-4" src="<?php echo $url ?>" /></div>
-                        <div class="col-lg-6 mb-3 mt-4 mt-md-0"><p class="text-white">&ldquo;<?php echo excerpt(100);?>&rdquo;</p><br><a class="text-white text-decoration-none align-content-end" href="<?php the_permalink() ?>">Read more →</a></div>
-                    </div>
-                </div><br>
-
-                <?php
-                endwhile; 
+            if($relatedPosts->have_posts()) :
+                while($relatedPosts->have_posts()) : $relatedPosts->the_post(); ?>
+                    <div class="container p-2 bg-dark text-white">
+                        <div class="row">
+                            <div class="col-lg-9">
+                                <h5><a class="text-decoration-none link-dark text-white" href='<?php the_permalink(); ?>'><?php the_title(); ?></a></h5>
+                            </div>
+                            <div class="col-lg-4">
+                                <?php $url = wp_get_attachment_url(get_post_thumbnail_id($post->ID)); ?>
+                                <img class="img-fluid rounded pb-xs-4" src="<?php echo $url ?>" />
+                            </div>
+                            <div class="col-lg-6 mb-3 mt-4 mt-md-0">
+                                <p class="text-white">&ldquo;<?php echo get_the_excerpt(); ?>&rdquo;</p>
+                                <br><a class="text-white text-decoration-none align-content-end" href="<?php the_permalink() ?>">Read more →</a>
+                            </div>
+                        </div>
+                    </div><br>
+                <?php endwhile;
                 wp_reset_query();
             endif;
-
-        endif; // end of the else statement from the first block
-
+        }
+    } else {  // This is the correct format for the else condition
+        $post_id = get_field('post_id');
+        $related_order_by = get_field('related_order_by', 'option');
+        $args = array(
+            'post_type' 			=> 'post',
+            'post_status' 			=> 'publish',
+            'orderby' 				=> $related_order_by,
+            'post__in'				=> $post_id
+        );
+        $relatedPosts = new WP_Query($args);
+        if ($relatedPosts-> have_posts()) :
+            while ($relatedPosts->have_posts()) : $relatedPosts->the_post();
+            ?>
+                <div class="container p-2 bg-dark text-white">
+                    <div class="row">
+                        <div class="col-lg-9">
+                            <h5><a class="text-decoration-none link-dark text-white" href='<?php echo get_permalink();?>'><?php the_title(); ?></a></h5>
+                        </div>
+                        <div class="col-lg-4">
+                            <?php $url = wp_get_attachment_url(get_post_thumbnail_id($post->ID)); ?>
+                            <img class="img-fluid rounded pb-xs-4" src="<?php echo $url ?>" />
+                        </div>
+                        <div class="col-lg-6 mb-3 mt-4 mt-md-0">
+                            <p class="text-white">&ldquo;<?php echo excerpt(100); ?>&rdquo;</p>
+                            <br><a class="text-white text-decoration-none align-content-end" href="<?php the_permalink() ?>">Read more →</a>
+                        </div>
+                    </div>
+                </div><br>
+            <?php
+            endwhile;
+            wp_reset_query();
+        endif;
     }
-}
-
-?>
+    ?>
 </div>
