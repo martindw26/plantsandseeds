43b44dcc37766c9b2e7bc2cde3259b3bded62d9a
Martin W <martindw97@gmail.com>
2023-08-09 22:59:17 +0100

    related_post_override v15

--- template-parts/related_new.php
+++ template-parts/related_new.php
@@ -2,9 +2,11 @@
     <h2>You may also <i>like</i></h2>
 
     <?php
+        // Get the post ID and the related order.
         $post_id = get_field('post_id');
         $related_order_by = get_field('related_order_by', 'option');
 
+        // If post_id exists, set the query arguments.
         if (!empty($post_id)) {
             $args = array(
                 'post_type' => 'post',
@@ -15,6 +17,7 @@
         } else {
             $related_post_override = get_field('related_post_override','option');
 
+            // If related post override is set to 'yes', adjust the query arguments.
             if ($related_post_override === 'yes') {
                 $current_category = get_the_category();
                 $category_id = $current_category[0]->cat_ID;
@@ -23,29 +26,30 @@
                     'post_type' => 'post',
                     'post_status' => 'publish',
                     'orderby' => $related_order_by,
-                    'category__in' => $category_id
+                    'category__in' => array($category_id)
                 );
             }
         }
 
+        // Perform the query.
         $relatedPosts = new WP_Query($args);
 
+        // Display related posts if available.
         if ($relatedPosts->have_posts()) :
             while ($relatedPosts->have_posts()) : $relatedPosts->the_post();
     ?>
 
     <div class="container p-2 bg-dark text-white">
         <div class="row">
-            <div class="col-lg-9">
+            <div class="col-lg-8">
                 <h5><a class="text-decoration-none link-dark text-white" href='<?php echo get_permalink(); ?>'><?php the_title(); ?></a></h5>
             </div>
             <div class="col-lg-4">
-                <?php $url = wp_get_attachment_url(get_post_thumbnail_id($post->ID)); ?>
-                <img class="img-fluid rounded pb-xs-4" src="<?php echo $url; ?>" />
+                <?php $url = wp_get_attachment_url(get_post_thumbnail_id(get_the_ID())); ?>
+                <img class="img-fluid rounded pb-xs-4" src="<?php echo esc_url($url); ?>" alt="<?php the_title(); ?>" />
             </div>
             <div class="col-lg-6 mb-3 mt-4 mt-md-0">
-                <p class="text-white">&ldquo;<?php echo excerpt(100); ?>&rdquo;</p>
-                <br>
+                <p class="text-white">&ldquo;<?php echo esc_html(get_the_excerpt()); ?>&rdquo;</p>
                 <a class="text-white text-decoration-none align-content-end" href="<?php the_permalink(); ?>">Read more →</a>
             </div>
         </div>
