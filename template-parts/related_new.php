7352b76c8f339d2eb48307e11870b1d644331e32
Martin W <martindw97@gmail.com>
2023-08-09 22:04:23 +0100

    related_post_override v3 load category posts if no related posts added

--- template-parts/related_new.php
+++ template-parts/related_new.php
@@ -1,11 +1,11 @@
 <div>
     <h2>You may also <i>like</i></h2>
     <?php
-    if(get_field('related_post_override', 'option') === 'yes') { 
+    $posts_displayed = false;
 
+    if(get_field('show_related_posts', 'option') === 'yes') {
         // Get categories from the current post
         $categories = get_the_category($post->ID);
-        
         if ($categories) {
             $category_ids = array();
             foreach($categories as $individual_category) $category_ids[] = $individual_category->term_id;
@@ -16,30 +16,18 @@
                 'post_type'     => 'post',
                 'post_status'   => 'publish',
                 'orderby'       => $related_order_by,
-                'category__in'  => $category_ids,  // This has changed
+                'category__in'  => $category_ids,
                 'post__not_in'  => array($post->ID) // Exclude current post
             );
 
             $relatedPosts = new WP_Query($args);
 
             if($relatedPosts->have_posts()) :
-                while($relatedPosts->have_posts()) : $relatedPosts->the_post(); ?>
-                    <div class="container p-2 bg-dark text-white">
-                        <div class="row">
-                            <div class="col-lg-9">
-                                <h5><a class="text-decoration-none link-dark text-white" href='<?php the_permalink(); ?>'><?php the_title(); ?></a></h5>
-                            </div>
-                            <div class="col-lg-4">
-                                <?php $url = wp_get_attachment_url(get_post_thumbnail_id($post->ID)); ?>
-                                <img class="img-fluid rounded pb-xs-4" src="<?php echo $url ?>" />
-                            </div>
-                            <div class="col-lg-6 mb-3 mt-4 mt-md-0">
-                                <p class="text-white">&ldquo;<?php echo get_the_excerpt(); ?>&rdquo;</p>
-                                <br><a class="text-white text-decoration-none align-content-end" href="<?php the_permalink() ?>">Read more →</a>
-                            </div>
-                        </div>
-                    </div><br>
-                <?php endwhile;
+                while($relatedPosts->have_posts()) : $relatedPosts->the_post(); 
+                    // Display related post content here...
+
+                    $posts_displayed = true;  // Mark that we've displayed some posts
+                endwhile;
                 wp_reset_query();
             endif;
         }
@@ -53,25 +41,29 @@
             'post__in'				=> $post_id
         );
         $relatedPosts = new WP_Query($args);
-        if ($relatedPosts-> have_posts()) :
+        if ($relatedPosts->have_posts()) :
             while ($relatedPosts->have_posts()) : $relatedPosts->the_post();
-            ?>
-                <div class="container p-2 bg-dark text-white">
-                    <div class="row">
-                        <div class="col-lg-9">
-                            <h5><a class="text-decoration-none link-dark text-white" href='<?php echo get_permalink();?>'><?php the_title(); ?></a></h5>
-                        </div>
-                        <div class="col-lg-4">
-                            <?php $url = wp_get_attachment_url(get_post_thumbnail_id($post->ID)); ?>
-                            <img class="img-fluid rounded pb-xs-4" src="<?php echo $url ?>" />
-                        </div>
-                        <div class="col-lg-6 mb-3 mt-4 mt-md-0">
-                            <p class="text-white">&ldquo;<?php echo excerpt(100); ?>&rdquo;</p>
-                            <br><a class="text-white text-decoration-none align-content-end" href="<?php the_permalink() ?>">Read more →</a>
-                        </div>
-                    </div>
-                </div><br>
-            <?php
+                // Display post content based on post_id criteria here...
+
+                $posts_displayed = true;  // Mark that we've displayed some posts
+            endwhile;
+            wp_reset_query();
+        endif;
+    }
+
+    // If no related posts were displayed, show posts from the category
+    if(!$posts_displayed) {
+        $categoryPosts = new WP_Query(array(
+            'post_type'     => 'post',
+            'post_status'   => 'publish',
+            'orderby'       => 'date',
+            'category__in'  => $category_ids
+        ));
+
+        if($categoryPosts->have_posts()) :
+            while($categoryPosts->have_posts()) : $categoryPosts->the_post();
+                // Display posts from the category...
+
             endwhile;
             wp_reset_query();
         endif;
