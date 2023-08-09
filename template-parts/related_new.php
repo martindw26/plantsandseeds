f1dfe5ae76cc81961bbb14fa5fd3e1999fb1f7eb
Martin W <martindw97@gmail.com>
2023-08-09 22:32:13 +0100

    related_post_override v10

--- template-parts/related_new.php
+++ template-parts/related_new.php
@@ -3,51 +3,49 @@
 <h2>You may also <i>like</i></h2>
 
 <?php
-
 $post_id = get_field('post_id');
 $related_order_by = get_field('related_order_by', 'option');
-$related_post_override = get_field('related_post_override');
-
 $args = array(
-    'post_type' => 'post',
-    'post_status' => 'publish',
-    'orderby' => $related_order_by,
-    'post__in' => $post_id
+	'post_type' => 'post',
+	'post_status' => 'publish',
+	'orderby' => $related_order_by,
+	'post__in' => $post_id
 );
 
 $relatedPosts = new WP_Query($args);
 
-// If no related posts and the override is set to 'yes', adjust the query to fetch posts from the same category.
 if (!$relatedPosts->have_posts() && $related_post_override == 'yes') {
-    $categories = wp_get_post_categories(get_the_ID());
-    $args['post__in'] = null;  // Reset post__in
-    $args['category__in'] = $categories;
-    $args['post__not_in'] = array(get_the_ID());  // Exclude current post
-    $relatedPosts = new WP_Query($args);
+	// Fetch posts based on the category of the current post
+	$categories = wp_get_post_categories(get_the_ID());
+	if (!empty($categories)) {
+		$args['category__in'] = $categories;
+		unset($args['post__in']); // Remove the post__in argument
+		$relatedPosts = new WP_Query($args);
+	}
 }
 
 if ($relatedPosts->have_posts()) :
-    while ($relatedPosts->have_posts()) : $relatedPosts->the_post();
-    ?>
-
-    <div class="container p-2 bg-dark text-white">
-        <div class="row">
-            <div class="col-lg-9"><h5><a class="text-decoration-none link-dark text-white" href='<?php echo get_permalink();?>'/></><?php the_title(); ?></a></h5></div>
-            <div class="col-lg-4"><?php $url = wp_get_attachment_url(get_post_thumbnail_id($post->ID)); ?>
-                <img class="img-fluid rounded pb-xs-4" src="<?php echo $url ?>" /></div>
-            <div class="col-lg-6 mb-3 mt-4 mt-md-0"><p class="text-white">&ldquo;<?php echo excerpt(100);?>&rdquo;</p><br><a class="text-white text-decoration-none align-content-end" href="<?php the_permalink() ?>">Read more →</a></div>
-        </div>
-    </div><br>
-
-    <?php
-    endwhile; 
-    wp_reset_postdata();
-
-else :
-    echo '<p>No related posts found.</p>';
-
-endif;
-
+	while ($relatedPosts->have_posts()) : $relatedPosts->the_post();
 ?>
 
+<div class="container p-2 bg-dark text-white">
+	<div class="row">
+		<div class="col-lg-9"><h5><a class="text-decoration-none link-dark text-white" href='<?php echo get_permalink();?>'><?php the_title(); ?></a></h5></div>
+		<div class="col-lg-4"><?php $url = wp_get_attachment_url(get_post_thumbnail_id($post->ID)); ?>
+			<img class="img-fluid rounded pb-xs-4" src="<?php echo $url ?>" />
+		</div>
+		<div class="col-lg-6 mb-3 mt-4 mt-md-0">
+			<p class="text-white">&ldquo;<?php echo excerpt(100);?>&rdquo;</p>
+			<br><a class="text-white text-decoration-none align-content-end" href="<?php the_permalink() ?>">Read more →</a>
+		</div>
+	</div>
+</div><br>
+
+<?php
+	endwhile;
+	wp_reset_query();
+else :
+	// Optionally add some content or message if no related posts are found
+endif;
+?>
 </div>
