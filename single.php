3a88e80c6fd828aea5bec42ab6c201b35079f74e
Martin W <martindw97@gmail.com>
2023-08-26 17:57:56 +0100

    test adding ads to articles

--- single.php
+++ single.php
@@ -15,7 +15,10 @@
        
 
             <h1 class=" display-2 font-weight-bold text-dark" style=""><?php echo esc_html( get_the_title() );?><hr></h1>
+             <!-- Desktop Header adslot-->
 
+<?php get_template_part( 'template-parts/landing_page_ads/homepage_header_top_slot_ad' );?><br>
+<!-- End Featured Post-->
             <p class="card-text" style="color: grey; font-size: small;">
                 By <?php the_author_meta('display_name', 1); ?> | Posted <?php echo get_the_date();?>
             </p>
