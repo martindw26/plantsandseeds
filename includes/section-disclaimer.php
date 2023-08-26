c94f41005ddf1741fcbcd0d2b3efc57de4ec4621
Martin W <martindw97@gmail.com>
2023-08-26 17:56:10 +0100

    &#9432;

--- includes/section-disclaimer.php
+++ includes/section-disclaimer.php
@@ -5,7 +5,7 @@ $disclaimer_url = get_field( 'article_post_content_disclaimer_block_url','option
 if(get_field('article_post_content_disclaimer_block_text','option'))
 {
     echo '<br><hr><br><div class="alert alert-warning alert-dismissible fade show" role="alert">
-        <span class="font-weight-normal text-danger">' . $disclaimer_text . '<a class="text-decoration-none" href='.$disclaimer_url.' target="_blank"/> &#128712; </a></span>
+        <span class="font-weight-normal text-danger">' . $disclaimer_text . '<a class="text-decoration-none" href='.$disclaimer_url.' target="_blank"/> &#9432; </a></span>
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>';
 }
