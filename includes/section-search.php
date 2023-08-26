f8f843db0f9ec290a38ab571f4c42677c2ebbdf1
Martin W <martindw97@gmail.com>
2023-08-26 18:14:48 +0100

    updated search

--- includes/section-search.php
+++ includes/section-search.php
@@ -1,8 +1,9 @@
 
 <form action="/" method="get">
-<div class="input-group mb-3 mt-2">
-  <input type="text" class="form-control rounded rounded-0" placeholder="Search" name="s" id="search" value=" <?php get_search_form(); ?>">
-  <button class="btn btn-danger rounded rounded-0" type="submit" >Go</button>
-</div>
+  <div class="input-group mb-3 mt-2">
+    <input type="text" class="form-control rounded rounded-0" placeholder="Search" name="s" id="search" value="<?php get_search_form(); ?>">
+    <button class="btn btn-danger rounded rounded-0" type="submit">Go</button>
+  </div>
 </form>
+
 
