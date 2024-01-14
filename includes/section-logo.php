680756544c71bac1f0fef8f9372c5f3d085cf959
martin williams <86750566+martindw26@users.noreply.github.com>
2024-01-14 23:47:06 +0000

    Updated logo URL

--- includes/section-logo.php
+++ includes/section-logo.php
@@ -1,11 +1,11 @@
 <div class="desktop-logo">
-<a href="https://techhobbyist.co.uk"/>
+<a href="https://plantsandseeds.co.uk/"/>
 <img src="<?php the_field ('site_logo_desktop', 'option');?>" class="img-fluid bg-dark border-0"</>
 </a>
 </div>
 
 <div class="mobile-logo">
-<a href="https://techhobbyist.co.uk"/>
+<a href="https://plantsandseeds.co.uk/"/>
 <img src="<?php the_field ('site_logo_mobile', 'option');?>" class="img-fluid bg-dark border-0"</>
 </a>
 </div>
