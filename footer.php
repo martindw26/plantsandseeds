fc77f1fc23eca38ad63e3f5a5c218221bd1cbbfe
martin williams <86750566+martindw26@users.noreply.github.com>
2024-01-16 18:22:55 +0000

    Added footer site settings

--- footer.php
+++ footer.php
@@ -1,17 +1,42 @@
-
-
-
 <?php 
 /* footer */
 ?>
 
-
-
 <footer>
 
+<?php
+$footer_background_color = get_field('footer_background_color', 'option');
+$footer_text_color = get_field('footer_text_color', 'option'); // Add this line to get the text color
 
+if ($footer_background_color || $footer_text_color) {
+  $style = 'style="';
+  // Check if background color is set, and if so, append it to the style
+  if ($footer_background_color) {
+      $style .= 'background-color: ' . esc_attr($footer_background_color) . ';';
+  }
+  // Check if text color is set, and if so, append it to the style
+  if ($footer_text_color) {
+    $style .= 'color: ' . esc_attr($footer_text_color) . ';';
+  }
+  $style .= '"';
+  echo '<nav class="footer_background navbar-fluid navbar-expand-md pt-1" ' . $style . ' role="navigation">';
+} else {
+  echo '<nav class="footer_background navbar-fluid navbar-expand-md pt-1" role="navigation">';
+} 
 
-<div class="container-fluid mt-3 text-decoration-none">
+/* Add this style block in your HTML head or in your stylesheet */
+?>
+<style>
+  .footer_text_color {
+    <?php
+    // Apply the text color to the nav-text based on the $navbar_text_color variable
+    if ($footer_text_color) {
+        echo 'color: ' . esc_attr($footer_text_color) . ';';
+    }
+    ?>
+  }
+</style>
+
   <div class="row">
     <div class="col-lg col-sm-4 p-3 text-decoration-none">
     	<?php
@@ -36,7 +61,8 @@
     </div>
     <div class="col-lg col-sm-6 p-3 text-lg-end text-sm-center">
 
-<p class="text-white"> Copyright <?php
+<p class="footer_text_color"> Copyright 
+<?php
 $current_year = date('Y');
 echo $current_year;
 ?> plantsandseeds.co.uk All Rights Reserved.</p>
