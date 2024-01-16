290d96c0758f2a937fd3b7ab85a74413677acfa4
martin williams <86750566+martindw26@users.noreply.github.com>
2024-01-16 23:52:12 +0000

    fix footer code

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /* footer */
 ?>
 
@@ -12,74 +12,64 @@ if ($footer_background_color || $footer_text_color) {
   $style = 'style="';
   // Check if background color is set, and if so, append it to the style
   if ($footer_background_color) {
-      $style .= 'background-color: ' . esc_attr($footer_background_color) . ';';
+    $style .= 'background-color: ' . esc_attr($footer_background_color) . ';';
   }
   // Check if text color is set, and if so, append it to the style
   if ($footer_text_color) {
     $style .= 'color: ' . esc_attr($footer_text_color) . ';';
   }
   $style .= '"';
-  echo '<div class="container-fluid mt-3 text-decoration-none">" ' . $style . '';
+  echo '<div class="container-fluid mt-3 text-decoration-none" ' . $style . '>';
 } else {
   echo '<div class="container-fluid mt-3 text-decoration-none">';
 } 
-
-/* Add this style block in your HTML head or in your stylesheet */
 ?>
+
+  <div class="row">
+    <div class="col-lg col-sm-4 p-3 text-decoration-none">
+    	<?php
+			wp_nav_menu(
+				array(
+					'theme_location' => 'footer_one',
+					'menu_class' => 'primary-menu-footer'
+				)	
+			);
+		?>
+    </div>
+    <div class="col-lg col-sm-4 p-3 text-decoration-none">
+    	<?php
+			wp_nav_menu(
+				array(
+					'theme_location' => 'footer_two',
+					'menu_class' => 'primary-menu-footer'
+				)	
+			);
+		?>
+    </div>
+    <div class="col-lg col-sm-6 p-3 text-lg-end text-sm-center">
+      <p class="footer_text_color"> Copyright <?php echo date('Y'); ?> plantsandseeds.co.uk All Rights Reserved.</p>
+      <button class="tracking-opt-out" onclick="openTrackingOptOutPage()">Tracking opt-out</button>
+    </div>
+  </div>
+</div>
+
 <style>
   .footer_text_color {
     <?php
     // Apply the text color to the nav-text based on the $navbar_text_color variable
     if ($footer_text_color) {
-        echo 'color: ' . esc_attr($footer_text_color) . ';';
+      echo 'color: ' . esc_attr($footer_text_color) . ';';
     }
     ?>
   }
 </style>
 
-  <div class="row">
-    <div class="col-lg col-sm-4 p-3 text-decoration-none">
-    	<?php
-					wp_nav_menu(
-						array(
-						'theme_location' => 'footer_one',
-						'menu_class' => 'primary-menu-footer'
-						)	
-					);
-					?>
-
-    </div>
-    <div class="col-lg col-sm-4 p-3 text-decoration-none">
-    	<?php
-					wp_nav_menu(
-						array(
-						'theme_location' => 'footer_two',
-						'menu_class' => 'primary-menu-footer'
-						)	
-					);
-					?>
-    </div>
-    <div class="col-lg col-sm-6 p-3 text-lg-end text-sm-center">
-
-<p class="footer_text_color"> Copyright 
-<?php
-$current_year = date('Y');
-echo $current_year;
-?> plantsandseeds.co.uk All Rights Reserved.</p>
-<button class="tracking-opt-out" onclick="openTrackingOptOutPage()">Tracking opt-out</button>
-
 <script>
     function openTrackingOptOutPage() {
-       
         var optOutUrl = "https://plantsandseeds.co.uk/tracking/tracking-opt-out/";
-
- 
         window.open(optOutUrl, "_blank");
     }
 </script>
-    </div>
-  </div>
-</div>
 
 </footer>
 
