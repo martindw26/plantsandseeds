89c5b85326fbc3986d84a11bffcd7e2d010117b7
martin williams <86750566+martindw26@users.noreply.github.com>
2024-01-17 01:02:18 +0000

    header code v4

--- includes/section-logo.php
+++ includes/section-logo.php
@@ -3,22 +3,20 @@ $header_background_color = get_field('header_background_color', 'option');
 
 // Check if the color value is not empty
 if ($header_background_color) {
-    echo '<div class="desktop-logo" style="background-color: ' . esc_attr($header_background_color) . ';">';
-    echo '<div class="mobile-logo" style="background-color: ' . esc_attr($header_background_color) . ';">';
+    $style_attribute = 'style="background-color: ' . esc_attr($header_background_color) . ';"';
 } else {
-    echo '<div class="desktop-logo">';
-    echo '<div class="mobile-logo">';
+    $style_attribute = ''; // No background color if not set
 }
 ?>
 
-<a href="https://plantsandseeds.co.uk/">
-    <img src="<?php the_field('site_logo_desktop', 'option'); ?>" class="img-fluid bg-dark border-0" />
-</a>
+<div class="desktop-logo" <?php echo $style_attribute; ?>>
+    <a href="https://plantsandseeds.co.uk/">
+        <img src="<?php the_field('site_logo_desktop', 'option'); ?>" class="img-fluid bg-dark border-0" alt="Desktop Logo" />
+    </a>
 </div>
 
-<a href="https://plantsandseeds.co.uk/">
-    <img src="<?php the_field('site_logo_mobile', 'option'); ?>" class="img-fluid bg-dark border-0" />
-</a>
-</div>
-
+<div class="mobile-logo" <?php echo $style_attribute; ?>>
+    <a href="https://plantsandseeds.co.uk/">
+        <img src="<?php the_field('site_logo_mobile', 'option'); ?>" class="img-fluid bg-dark border-0" alt="Mobile Logo" />
+    </a>
 </div>
