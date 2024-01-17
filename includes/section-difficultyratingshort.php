271bb1d1e58597912a6416a94e317aa3d588aea9
martin williams <86750566+martindw26@users.noreply.github.com>
2024-01-17 03:24:35 +0000

    dashicons-carrot

--- includes/section-difficultyratingshort.php
+++ includes/section-difficultyratingshort.php
@@ -7,19 +7,19 @@
                                 echo '<style>div#rating_category { display:none;}</style>';
                                 break;
                               case "1":
-                                echo '<div id="rating_statement_difficulty"><p><p class="dashicons dashicons-hammer"></p> Easy </p></div>';
+                                echo '<div id="rating_statement_difficulty"><p><p class="dashicons dashicons-carrot"></p> Easy </p></div>';
                                 break;
                               case "2":
-                                echo '<div id="rating_statement_difficulty"><p><p class="dashicons dashicons-hammer"></p> Easy / Intermediate</p></div>';
+                                echo '<div id="rating_statement_difficulty"><p><p class="dashicons dashicons-carrot"></p> Easy / Intermediate</p></div>';
                                 break;
                               case "3":
-                                echo '<div id="rating_statement_difficulty"><p><p class="dashicons dashicons-hammer"></p> Intermediate / Difficult</p></div>';
+                                echo '<div id="rating_statement_difficulty"><p><p class="dashicons dashicons-carrot"></p> Intermediate / Difficult</p></div>';
                                 break;
                               case "4":
-                                echo '<div id="rating_statement_difficulty"><p><p class="dashicons dashicons-hammer fs-5 text"></p> Difficult</p></div>';                                
+                                echo '<div id="rating_statement_difficulty"><p><p class="dashicons dashicons-carrot fs-5 text"></p> Difficult</p></div>';                                
                                 break;
                          	  case "5":
-                                echo '<div id="rating_statement_difficulty"><p><p class="dashicons dashicons-hammer"></p> Very Difficult</p></div>';
+                                echo '<div id="rating_statement_difficulty"><p><p class="dashicons dashicons-carrot"></p> Very Difficult</p></div>';
                                 break;
                              default:
                                 echo '<style>div#rating_category { display:none;}</style>';
