01608b55301b4d5c22553ec27ec5151469b37155
martin williams <86750566+martindw26@users.noreply.github.com>
2024-01-17 00:59:25 +0000

    header code v2

--- includes/section-logo.php
+++ includes/section-logo.php
@@ -1,32 +1,22 @@
 <?php
-$header_background_color = get_field('header_background_color' ,'option');
+$header_background_color = get_field('header_background_color', 'option');
 
 // Check if the color value is not empty
 if ($header_background_color) {
-
     echo '<div class="desktop-logo" style="background-color: ' . esc_attr($header_background_color) . ';">';
+    echo '<a href="https://plantsandseeds.co.uk/"><img src="' . esc_url(the_field('site_logo_desktop', 'option')) . '" class="img-fluid bg-dark border-0"></a>';
+    echo '</div>';
+
     echo '<div class="mobile-logo" style="background-color: ' . esc_attr($header_background_color) . ';">';
+    echo '<a href="https://plantsandseeds.co.uk/"><img src="' . esc_url(the_field('site_logo_mobile', 'option')) . '" class="img-fluid bg-dark border-0"></a>';
+    echo '</div>';
 } else {
     echo '<div class="desktop-logo">';
+    echo '<a href="https://plantsandseeds.co.uk/"><img src="' . esc_url(the_field('site_logo_desktop', 'option')) . '" class="img-fluid bg-dark border-0"></a>';
+    echo '</div>';
+
     echo '<div class="mobile-logo">';
+    echo '<a href="https://plantsandseeds.co.uk/"><img src="' . esc_url(the_field('site_logo_mobile', 'option')) . '" class="img-fluid bg-dark border-0"></a>';
+    echo '</div>';
 }
 ?>
-
-<div class="desktop-logo">
-<a href="https://plantsandseeds.co.uk/"/>
-<img src="<?php the_field ('site_logo_desktop', 'option');?>" class="img-fluid bg-dark border-0"</>
-</a>
-</div>
-
-<div class="mobile-logo">
-<a href="https://plantsandseeds.co.uk/"/>
-<img src="<?php the_field ('site_logo_mobile', 'option');?>" class="img-fluid bg-dark border-0"</>
-</a>
-</div>
-
-
-</div>
-
-
-
-
