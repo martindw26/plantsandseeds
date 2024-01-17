05c281e3e8409172772b269c485a23fdc967228f
martin williams <86750566+martindw26@users.noreply.github.com>
2024-01-17 03:18:00 +0000

    Growing difficulty rating:

--- includes/section-difficulty_rating.php
+++ includes/section-difficulty_rating.php
@@ -4,19 +4,19 @@
                                 echo '<style>div#rating_category { display:none;}</style>';
                                 break;
                               case "1":
-                                echo '<div id="rating_statement"><h5> Project difficulty rating:  Easy </h5></div>';
+                                echo '<div id="rating_statement"><h5> Growing difficulty rating:  Easy </h5></div>';
                                 break;
                               case "2":
-                                echo '<div id="rating_statement"><h5> Project difficulty rating:  Easy / Intermediate</h5></div>';
+                                echo '<div id="rating_statement"><h5> Growing difficulty rating:  Easy / Intermediate</h5></div>';
                                 break;
                               case "3":
-                                echo '<div id="rating_statement"><h5> Project difficulty rating:  Intermediate / Difficult</h5></div>';
+                                echo '<div id="rating_statement"><h5> Growing difficulty rating:  Intermediate / Difficult</h5></div>';
                                 break;
                               case "4":
-                                echo '<div id="rating_statement"><h5> Project difficulty rating:  Difficult</h5></div>';                                
+                                echo '<div id="rating_statement"><h5> Growing difficulty rating:  Difficult</h5></div>';                                
                                 break;
                          	  case "5":
-                                echo '<div id="rating_statement"><h5> Project difficulty rating:  Very Difficult</h5></div>';
+                                echo '<div id="rating_statement"><h5> Growing difficulty rating:  Very Difficult</h5></div>';
                                 break;
                              default:
                                 echo '<style>div#rating_category { display:none;}</style>';
